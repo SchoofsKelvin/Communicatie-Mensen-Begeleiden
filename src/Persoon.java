84c2c4a4103490ea06bac72b6a0c336747b46bcb
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2017-03-28 11:38:15 +0200

    Maak Persoon klasse

---
+++ src/Persoon.java
@@ -0,0 +1,27 @@
+
+public class Persoon {
+
+	private String	voornaam;
+	private String	achternaam;
+
+	public Persoon(String voornaam, String achternaam) {
+		this.voornaam = voornaam;
+		this.achternaam = achternaam;
+	}
+
+	public String getAchternaam() {
+		return achternaam;
+	}
+
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public void setAchternaam(String achternaam) {
+		this.achternaam = achternaam;
+	}
+
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+}
