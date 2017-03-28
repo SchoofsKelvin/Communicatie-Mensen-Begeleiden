03527639da0e35307785b68082b8014413b52c93
lenniwijnants <wijnantslenni@hotmail.com>
2017-03-28 11:58:03 +0200

    basic frame toegevoegd

---
+++ src/KlasFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class KlasFrame extends JFrame{
+
+	public KlasFrame(Klas klas){
+		super("Klasprogramma");
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	}
+	
+}
--- src/KlasProgramma.java
+++ src/KlasProgramma.java
@@ -5,6 +5,8 @@ public class KlasProgramma {
 		Klas klasje = new Klas("Klasje 1-A");
 		Persoon leerling1 = new Persoon("John", "Smith");
 		klasje.addStudent(leerling1);
+		
+		
 	}
 
 }
